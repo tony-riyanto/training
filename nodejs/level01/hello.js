dfa7205e4c8172c3406a547e67810d063bd454cc
tony-riyanto <tony.riyanto@gmail.com>
2015-07-09 08:03:46 +0200

    init

---
+++ nodejs/level01/hello.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Salut tout le monde !');
+});
+console.log("coco");
+server.listen(8080);
